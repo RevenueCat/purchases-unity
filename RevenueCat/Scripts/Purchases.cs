263185c83b5846640d90aff1200948b8eeedcd0b
Cesar de la Vega <cesarvegaro@gmail.com>
2020-04-17 16:25:55 -0700

    Moves ProrationMode

--- RevenueCat/Scripts/Purchases.cs
+++ RevenueCat/Scripts/Purchases.cs
@@ -9,28 +9,6 @@ using RevenueCat.MiniJSON;
 
 public partial class Purchases : MonoBehaviour
 {
-    public enum ProrationMode
-    {
-        UnknownSubscriptionUpgradeDowngradePolicy = 0,
-
-        /// Replacement takes effect immediately, and the remaining time will be
-        /// prorated and credited to the user. This is the current default behavior.
-        ImmediateWithTimeProration = 1,
-
-        /// Replacement takes effect immediately, and the billing cycle remains the
-        /// same. The price for the remaining period will be charged. This option is
-        /// only available for subscription upgrade.
-        ImmediateAndChargeProratedPrice = 2,
-
-        /// Replacement takes effect immediately, and the new price will be charged on
-        /// next recurrence time. The billing cycle stays the same.
-        ImmediateWithoutProration = 3,
-
-        /// Replacement takes effect when the old plan expires, and the new price will
-        /// be charged at the same time.
-        Deferred = 4
-    }
-
     public enum IntroEligibilityStatus
     {
         /// RevenueCat doesn't have enough information to determine eligibility.
