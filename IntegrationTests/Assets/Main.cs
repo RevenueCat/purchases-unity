a666b30d31d8046a2a5e8db8bfc0a3b86cb50765
Cesar de la Vega <cesarvegaro@gmail.com>
2020-12-22 12:42:31 -0800

    Integration tests (#35)
    * Adds integration tests
    
    * comments cleanup
    
    * PR comments
    
    * add filters
    
    * fixes config

---
+++ IntegrationTests/Assets/Main.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    
+    // Use this for initialization
+    private void Start()
+    {
+        var purchases = GetComponent<Purchases>();
+        purchases.SetDebugLogsEnabled(true);
+        
+        purchases.GetPurchaserInfo((info, error) =>
+        {
+            
+        });
+    }
+
+}
