1839f04890a7598e94b8532658af4e0dd33ff5b4
César de la Vega <cesarvegaro@gmail.com>
2021-01-29 09:57:59 -0800

    updates Product

--- RevenueCat/Scripts/Product.cs
+++ RevenueCat/Scripts/Product.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using RevenueCat.SimpleJSON;
@@ -12,11 +13,20 @@ public partial class Purchases
         public float price;
         public string priceString;
         [CanBeNull] public string currencyCode;
+        
+        public IntroductoryPrice IntroductoryPrice;
+        
+        [Obsolete("Use the IntroductoryPrice.Price property instead.")]
         public float introPrice;
+        [Obsolete("Use the IntroductoryPrice.PriceString property instead.")]
         public string introPriceString;
+        [Obsolete("Use the IntroductoryPrice.Period property instead.")]
         public string introPricePeriod;
+        [Obsolete("Use the IntroductoryPrice.Unit property instead.")]
         public string introPricePeriodUnit;
+        [Obsolete("Use the IntroductoryPrice.NumberOfUnits property instead.")]
         public int introPricePeriodNumberOfUnits;
+        [Obsolete("Use the IntroductoryPrice.Cycles property instead.")]
         public int introPriceCycles;
 
         public Product(JSONNode response)
@@ -27,12 +37,14 @@ public partial class Purchases
             price = response["price"];
             priceString = response["price_string"];
             currencyCode = response["currency_code"];
-            introPrice = response["intro_price"];
-            introPriceString = response["intro_price_string"];
-            introPricePeriod = response["intro_price_period"];
-            introPricePeriodUnit = response["intro_price_period_unit"];
-            introPricePeriodNumberOfUnits = response["intro_price_period_number_of_units"];
-            introPriceCycles = response["intro_price_cycles"];
+            
+            var introPriceJson = response["introPrice"];
+            introPrice = introPriceJson["price"];
+            introPriceString = introPriceJson["priceString"];
+            introPricePeriod = introPriceJson["period"];
+            introPricePeriodUnit = introPriceJson["periodUnit"];
+            introPricePeriodNumberOfUnits = introPriceJson["periodNumberOfUnits"];
+            introPriceCycles = introPriceJson["cycles"];
         }
 
         public override string ToString()
@@ -49,6 +61,36 @@ public partial class Purchases
                    $"{nameof(introPricePeriodUnit)}: {introPricePeriodUnit}, " +
                    $"{nameof(introPricePeriodNumberOfUnits)}: {introPricePeriodNumberOfUnits}, " +
                    $"{nameof(introPriceCycles)}: {introPriceCycles}";
+        }
+    }
+
+    public class IntroductoryPrice
+    {
+        public float Price;
+        public string PriceString;
+        public string Period;
+        public string Unit;
+        public int NumberOfUnits;
+        public int Cycles;
+        
+        public IntroductoryPrice(JSONNode response)
+        {
+           Price = response["price"];
+           PriceString = response["priceString"];
+           Period = response["period"];
+           Unit = response["periodUnit"];
+           NumberOfUnits = response["periodNumberOfUnits"];
+           Cycles = response["cycles"];
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Price)}: {Price}, " +
+                   $"{nameof(PriceString)}: {PriceString}, " +
+                   $"{nameof(Period)}: {Period}, " +
+                   $"{nameof(Unit)}: {Unit}, " +
+                   $"{nameof(NumberOfUnits)}: {NumberOfUnits}, " +
+                   $"{nameof(Cycles)}: {Cycles}";
         }
     }
 }
