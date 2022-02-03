ba5b2bf1a4a7010beed1d1e6ae20ed169e313d54
aboedo <andresboedo@gmail.com>
2022-02-03 18:01:12 -0300

    updated post install script to auto add storekit

--- RevenueCat/Editor/RevenueCatPostInstall.cs
+++ RevenueCat/Editor/RevenueCatPostInstall.cs
@@ -14,15 +14,14 @@ namespace Editor
             if (buildTarget == BuildTarget.iOS)
             {
                 ModifyFrameworks(path);
+                AddStoreKitFramework(path);
+                SaveProject(path);
             }
         }
  
         private static void ModifyFrameworks(string path)
         {
-            string projPath = PBXProject.GetPBXProjectPath(path);
-           
-            var project = new PBXProject();
-            project.ReadFromFile(projPath);
+            var project = GetProject(path);
  
             string mainTargetGuid = project.GetUnityMainTargetGuid();
            
@@ -32,8 +31,29 @@ namespace Editor
             }
            
             project.SetBuildProperty(mainTargetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
- 
+        }
+
+        private static void AddStoreKitFramework(string path)
+        {
+            var project = GetProject(path);
+            string mainTargetGUID = PBXProject.GetUnityMainTargetGuid();
+            project.AddFrameworkToProject(mainTargetGUID, "StoreKit", false);
+        }
+
+        private static PBXProject GetProject(string path)
+        {
+            string projPath = PBXProject.GetPBXProjectPath(path);
+            var project = new PBXProject();
+            project.ReadFromFile(projPath);
+            return project
+        }
+
+        private static void SaveProject(string path)
+        {
+            string projPath = PBXProject.GetPBXProjectPath(path);
+            var project = GetProject(path);
             project.WriteToFile(projPath);
         }
+        
     }
 }
