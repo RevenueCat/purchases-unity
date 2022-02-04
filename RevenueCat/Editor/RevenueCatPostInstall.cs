3fb469ee7b246a9ddc7f1ac18f7ce973606aa2f3
aboedo <andresboedo@gmail.com>
2022-02-04 11:26:44 -0300

    Auto add storekit (#85)
    * updated post install script to auto add storekit
    
    * fixed bugs in the script
    
    * added comment to explain the 999 magic number

--- RevenueCat/Editor/RevenueCatPostInstall.cs
+++ RevenueCat/Editor/RevenueCatPostInstall.cs
@@ -8,22 +8,23 @@ namespace Editor
 {
     public static class XcodeSwiftVersionPostProcess
     {
+        // set callbackOrder to 999 to ensure this runs as the last post process step
         [PostProcessBuild(999)]
         public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
         {
             if (buildTarget == BuildTarget.iOS)
             {
                 ModifyFrameworks(path);
+                AddStoreKitFramework(path);
             }
         }
  
         private static void ModifyFrameworks(string path)
         {
             string projPath = PBXProject.GetPBXProjectPath(path);
-           
             var project = new PBXProject();
             project.ReadFromFile(projPath);
- 
+
             string mainTargetGuid = project.GetUnityMainTargetGuid();
            
             foreach (var targetGuid in new[] { mainTargetGuid, project.GetUnityFrameworkTargetGuid() })
@@ -32,8 +33,21 @@ namespace Editor
             }
            
             project.SetBuildProperty(mainTargetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
- 
+
             project.WriteToFile(projPath);
         }
+
+        private static void AddStoreKitFramework(string path)
+        {
+            string projPath = PBXProject.GetPBXProjectPath(path);
+            var project = new PBXProject();
+            project.ReadFromFile(projPath);
+            
+            string mainTargetGUID = project.GetUnityMainTargetGuid();
+            project.AddFrameworkToProject(mainTargetGUID, "StoreKit.framework", false);
+            
+            project.WriteToFile(projPath);
+        }
+        
     }
 }
