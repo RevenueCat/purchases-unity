1f255b6caabec94cd89c2eb4d1e20f7b65ea5151
aboedo <andresboedo@gmail.com>
2022-02-04 11:21:59 -0300

    added post install script to prevent upload error (#84)
    * added post install script
    
    * added meta files

---
+++ RevenueCat/Editor/RevenueCatPostInstall.cs
@@ -0,0 +1,39 @@
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+ 
+namespace Editor
+{
+    public static class XcodeSwiftVersionPostProcess
+    {
+        [PostProcessBuild(999)]
+        public static void OnPostProcessBuild(BuildTarget buildTarget, string path)
+        {
+            if (buildTarget == BuildTarget.iOS)
+            {
+                ModifyFrameworks(path);
+            }
+        }
+ 
+        private static void ModifyFrameworks(string path)
+        {
+            string projPath = PBXProject.GetPBXProjectPath(path);
+           
+            var project = new PBXProject();
+            project.ReadFromFile(projPath);
+ 
+            string mainTargetGuid = project.GetUnityMainTargetGuid();
+           
+            foreach (var targetGuid in new[] { mainTargetGuid, project.GetUnityFrameworkTargetGuid() })
+            {
+                project.SetBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "NO");
+            }
+           
+            project.SetBuildProperty(mainTargetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+ 
+            project.WriteToFile(projPath);
+        }
+    }
+}
