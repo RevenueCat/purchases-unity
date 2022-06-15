9f433eb2aae9f1f3c160571f114390bb45d7e513
aboedo <andresboedo@gmail.com>
2022-06-15 17:47:32 -0300

    added a few API calls

--- IntegrationTests/Assets/Main.cs
+++ IntegrationTests/Assets/Main.cs
@@ -1,4 +1,8 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Purchasing;
 
 
 public class CustomListener : Purchases.UpdatedCustomerInfoListener
@@ -11,7 +15,6 @@ public class CustomListener : Purchases.UpdatedCustomerInfoListener
 
 public class Main : MonoBehaviour
 {
-    
     // Use this for initialization
     private void Start()
     {
@@ -26,17 +29,100 @@ public class Main : MonoBehaviour
         purchases.observerMode = true;
         purchases.userDefaultsSuiteName = "suitename";
         purchases.proxyURL = "https://proxy-url.revenuecat.com";
-        
-        
+
         Purchases.CustomerInfo receivedCustomerInfo;
         Purchases.Error receivedError;
+
+        Purchases.Package receivedPackage;
+        Purchases.Offerings receivedOfferings;
+        Purchases.Offering receivedOffering;
+        String receivedProductIdentifier;
+        bool receivedUserCancelled;
+
+        List<Purchases.StoreProduct> receivedProducts = new List<Purchases.StoreProduct>();
+
+        List<Purchases.StoreProduct> storeProducts = new List<Purchases.StoreProduct>();
+        purchases.GetProducts(new string[] { "a", "b" }, (products, error) => { receivedProducts = products; });
+
+        purchases.GetProducts(new string[] { "a", "b" }, (products, error) => { receivedProducts = products; }, "type");
+
+        purchases.GetOfferings((offerings, error) =>
+        {
+            receivedOfferings = offerings;
+            receivedError = error;
+            receivedOffering = receivedOfferings.All.First().Value;
+
+            receivedPackage = receivedOffering.AvailablePackages.First();
+            purchases.PurchasePackage(receivedPackage, (productIdentifier, customerInfo, userCancelled, error2) =>
+            {
+                receivedProductIdentifier = productIdentifier;
+                receivedCustomerInfo = customerInfo;
+                receivedUserCancelled = userCancelled;
+                receivedError = error2;
+            });
+
+            purchases.PurchasePackage(receivedPackage, (productIdentifier, customerInfo, userCancelled, error2) =>
+            {
+                receivedProductIdentifier = productIdentifier;
+                receivedCustomerInfo = customerInfo;
+                receivedUserCancelled = userCancelled;
+                receivedError = error2;
+            }, "oldSku", Purchases.ProrationMode.Deferred);
+
+            Purchases.StoreProduct storeProduct = storeProducts.First();
+            Purchases.PromotionalOffer receivedPromoOffer;
+            purchases.GetPromotionalOffer(storeProduct, storeProduct.discounts.First(), (offer, error2) =>
+            {
+                receivedPromoOffer = offer;
+                receivedError = error2;
+
+                purchases.PurchaseDiscountedPackage(receivedPackage, receivedPromoOffer,
+                    (productIdentifier, purchaserInfo, userCancelled, error3) =>
+                    {
+                        receivedProductIdentifier = productIdentifier;
+                        receivedCustomerInfo = purchaserInfo;
+                        receivedUserCancelled = userCancelled;
+                        receivedError = error3;
+                    });
+                
+                purchases.PurchaseDiscountedProduct("product_id", receivedPromoOffer,
+                    (productIdentifier, purchaserInfo, userCancelled, error3) =>
+                    {
+                        receivedProductIdentifier = productIdentifier;
+                        receivedCustomerInfo = purchaserInfo;
+                        receivedUserCancelled = userCancelled;
+                        receivedError = error3;
+                    });
+            });
+        });
+
+        purchases.PurchaseProduct("product_id", (productIdentifier, customerInfo, userCancelled, error2) =>
+        {
+            receivedProductIdentifier = productIdentifier;
+            receivedCustomerInfo = customerInfo;
+            receivedUserCancelled = userCancelled;
+            receivedError = error2;
+        });
+
+        purchases.PurchaseProduct("product_id", (productIdentifier, purchaserInfo, userCancelled, error2) =>
+        {
+            receivedProductIdentifier = productIdentifier;
+            receivedCustomerInfo = purchaserInfo;
+            receivedUserCancelled = userCancelled;
+            receivedError = error2;
+        }, "type", "oldSku", Purchases.ProrationMode.Deferred);
+
         purchases.GetCustomerInfo((info, error) =>
         {
             receivedCustomerInfo = info;
             receivedError = error;
         });
         
+        purchases.RestorePurchases((customerInfo, error) =>
+        {
+            receivedCustomerInfo = customerInfo;
+            receivedError = error;
+        });
         
     }
-
 }
