ad3c9375c78c891edfd760f8006f943dd123a9e7
aboedo <andresboedo@gmail.com>
2022-06-15 16:57:53 -0300

    added test calls to a few public properties

--- IntegrationTests/Assets/Main.cs
+++ IntegrationTests/Assets/Main.cs
@@ -1,18 +1,42 @@
 ﻿using UnityEngine;
 
+
+public class CustomListener : Purchases.UpdatedCustomerInfoListener
+{
+    public override void CustomerInfoReceived(Purchases.CustomerInfo customerInfo)
+    {
+        throw new System.NotImplementedException();
+    }
+}
+
 public class Main : MonoBehaviour
 {
     
     // Use this for initialization
     private void Start()
     {
-        var purchases = GetComponent<Purchases>();
+        Purchases purchases = GetComponent<Purchases>();
         purchases.SetDebugLogsEnabled(true);
+        purchases.deprecatedLegacyRevenueCatAPIKey = "abc";
+        purchases.revenueCatAPIKeyApple = "def";
+        purchases.revenueCatAPIKeyGoogle = "ghi";
+        purchases.appUserID = "abc";
+        purchases.productIdentifiers = new[] { "a", "b", "c" };
+        purchases.listener = new CustomListener();
+        purchases.observerMode = true;
+        purchases.userDefaultsSuiteName = "suitename";
+        purchases.proxyURL = "https://proxy-url.revenuecat.com";
         
+        
+        Purchases.CustomerInfo receivedCustomerInfo;
+        Purchases.Error receivedError;
         purchases.GetCustomerInfo((info, error) =>
         {
-            
+            receivedCustomerInfo = info;
+            receivedError = error;
         });
+        
+        
     }
 
 }
