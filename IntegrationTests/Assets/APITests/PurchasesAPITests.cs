1e168bddb28f204d7185c6ee71d99abb40e11897
Cesar de la Vega <cesarvegaro@gmail.com>
2023-02-22 15:27:38 -0800

    remove import

--- IntegrationTests/Assets/APITests/PurchasesAPITests.cs
+++ IntegrationTests/Assets/APITests/PurchasesAPITests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class CustomListener : Purchases.UpdatedCustomerInfoListener
