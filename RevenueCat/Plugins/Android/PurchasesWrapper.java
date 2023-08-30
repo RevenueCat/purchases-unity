778eca49481ecfd9cef61bd5fb6ea4ba394d88c1
RCGitBot <dev+RCGitBot@revenuecat.com>
2023-08-30 22:39:02 +0000

    Preparing for next version

--- RevenueCat/Plugins/Android/PurchasesWrapper.java
+++ RevenueCat/Plugins/Android/PurchasesWrapper.java
@@ -45,7 +45,7 @@ public class PurchasesWrapper {
     private static final String HANDLE_LOG = "_handleLog";
 
     private static final String PLATFORM_NAME = "unity";
-    private static final String PLUGIN_VERSION = "5.1.2";
+    private static final String PLUGIN_VERSION = "5.2.0-SNAPSHOT";
 
     private static String gameObject;
 
