0354d6c640d0b485781afabd258e2927e6294062
NachoSoto <ignaciosoto90@gmail.com>
2023-11-10 11:04:26 -0800

    release/3.5.4
    - Update `purchases-ios` to version [3.14.4](https://github.com/RevenueCat/purchases-ios/releases/tag/3.14.4)

--- RevenueCat/Plugins/Android/PurchasesWrapper.java
+++ RevenueCat/Plugins/Android/PurchasesWrapper.java
@@ -46,7 +46,7 @@ public class PurchasesWrapper {
     private static final String GET_PAYMENT_DISCOUNT = "_getPaymentDiscount";
 
     private static final String PLATFORM_NAME = "unity";
-    private static final String PLUGIN_VERSION = "3.5.3";
+    private static final String PLUGIN_VERSION = "3.5.4";
 
     private static String gameObject;
     private static UpdatedPurchaserInfoListener listener = new UpdatedPurchaserInfoListener() {
