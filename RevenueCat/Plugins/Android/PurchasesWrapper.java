RevenueCat/purchases-unity

commit fd06d3d1e9cd019168f7442b82e7e9216f5ee609

Support Web Redemption Links (#539)
This adds support for Web Redemption Links in our Unity SDK.

RevenueCat/Plugins/Android/PurchasesWrapper.java
@@ -48,6 +48,8 @@ public class PurchasesWrapper {
     private static final String GET_PROMOTIONAL_OFFER = "_getPromotionalOffer";
     private static final String GET_LWA_CONSENT_STATUS = "_getAmazonLWAConsentStatus";
     private static final String SYNC_PURCHASES = "_syncPurchases";
+    private static final String PARSE_AS_WEB_PURCHASE_REDEMPTION = "_parseAsWebPurchaseRedemption";
+    private static final String REDEEM_WEB_PURCHASE = "_redeemWebPurchase";
 
     private static final String HANDLE_LOG = "_handleLog";
 
@@ -563,6 +565,35 @@ public class PurchasesWrapper {
         } catch (JSONException e) {
             logJSONException(e);
         }
+    }
+
+    public static void parseAsWebPurchaseRedemption(String urlString) {
+        boolean isWebPurchaseRedemptionURL = CommonKt.isWebPurchaseRedemptionURL(urlString);
+        if (isWebPurchaseRedemptionURL) {
+            JSONObject object = new JSONObject();
+            object.put("redemptionLink", urlString);
+            sendJSONObject(object, PARSE_AS_WEB_PURCHASE_REDEMPTION);
+        } else {
+            sendJSONObject(null, PARSE_AS_WEB_PURCHASE_REDEMPTION);
+        }
+    }
+
+    public static void redeemWebPurchase(String redemptionLink) {
+        CommonKt.redeemWebPurchase(redemptionLink, new OnResult() {
+            @Override
+            public void onReceived(Map<String, ?> map) {
+                try {
+                    sendJSONObject(MappersHelpersKt.convertToJson(map), REDEEM_WEB_PURCHASE);
+                } catch (JSONException e) {
+                    logJSONException(e);
+                }
+            }
+
+            @Override
+            public void onError(ErrorContainer errorContainer) {
+                sendError(errorContainer, REDEEM_WEB_PURCHASE);
+            }
+        });
     }
 
     private static void logJSONException(JSONException e) {

RevenueCat/Plugins/Editor/RevenueCatDependencies.xml
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <dependencies>
     <androidPackages>
-        <androidPackage spec="com.revenuecat.purchases:purchases-hybrid-common:[13.13.0]" />
+        <androidPackage spec="com.revenuecat.purchases:purchases-hybrid-common:[13.14.0]" />
         <androidPackage spec="androidx.annotation:annotation:[1.2.0]" />
     </androidPackages>
     <iosPods>
-        <iosPod name="PurchasesHybridCommon" version="13.13.0" minTargetSdk="13.0"/>
+        <iosPod name="PurchasesHybridCommon" version="13.14.0" minTargetSdk="13.0"/>
     </iosPods>
 </dependencies>

RevenueCat/Plugins/iOS/PurchasesUnityHelper.m
@@ -26,6 +26,8 @@ static NSString *const GET_PROMOTIONAL_OFFER = @"_getPromotionalOffer";
 static NSString *const HANDLE_LOG = @"_handleLog";
 static NSString *const RECORD_PURCHASE = @"_recordPurchase";
 static NSString *const SYNC_PURCHASES = @"_syncPurchases";
+static NSString *const PARSE_AS_WEB_PURCHASE_REDEMPTION = @"_parseAsWebPurchaseRedemption";
+static NSString *const REDEEM_WEB_PURCHASE = @"_redeemWebPurchase";
 
 #pragma mark Utility Methods
 
@@ -440,6 +442,36 @@ signedDiscountTimestamp:(NSString *)signedDiscountTimestamp {
     #endif
 }
 
+- (void)parseAsWebPurchaseRedemption:(NSString *)urlString {
+    BOOL isWebPurchaseRedemptionURL = [RCCommonFunctionality isWebPurchaseRedemptionURL:urlString];
+    if (isWebPurchaseRedemptionURL) {
+        [self sendJSONObject:@{@"redemptionLink": urlString} toMethod:PARSE_AS_WEB_PURCHASE_REDEMPTION];
+    } else {
+        [self sendJSONObject:nil toMethod:PARSE_AS_WEB_PURCHASE_REDEMPTION];
+    }
+}
+
+- (void)redeemWebPurchase:(NSString *)redemptionLink {
+    [RCCommonFunctionality redeemWebPurchaseWithUrlString:redemptionLink
+                                               completion:^(NSDictionary *_Nullable responseDictionary,
+                                                            RCErrorContainer *_Nullable error) {
+        if (error == nil && responseDictionary == nil) {
+            NSError *nsError = [[NSError alloc] initWithDomain:RCPurchasesErrorCodeDomain
+                                                          code:RCUnknownError
+                                                      userInfo:@{NSLocalizedDescriptionKey: @"Both error and response are null"}];
+            error = [[RCErrorContainer alloc] initWithError:nsError extraPayload:@{}];
+        }
+
+        NSDictionary *response = (error)
+        ? @{
+            @"error": error.info
+        }
+        : responseDictionary;
+
+        [self sendJSONObject:response toMethod:REDEEM_WEB_PURCHASE];
+    }];
+}
+
 #pragma mark Helper Methods
 
 - (void)sendJSONObject:(NSDictionary *)jsonObject toMethod:(NSString *)methodName {
@@ -783,4 +815,12 @@ void _RCShowInAppMessages(const char *messagesJSON) {
     }
 
     [_RCUnityHelperShared() showInAppMessages:messagesDictionary[@"messageTypes"]];
+}
+
+void _RCParseAsWebPurchaseRedemption(const char *urlString) {
+    [_RCUnityHelperShared() parseAsWebPurchaseRedemption:convertCString(urlString)];
+}
+
+void _RCRedeemWebPurchase(const char *redemptionLink) {
+    [_RCUnityHelperShared() redeemWebPurchase:convertCString(redemptionLink)];
 }

RevenueCat/Scripts/Purchases.cs
@@ -1251,6 +1251,26 @@ public partial class Purchases : MonoBehaviour
         _wrapper.ShowInAppMessages(messageTypes);
     }
 
+    public delegate void ParseAsWebPurchaseRedemptionFunc(WebPurchaseRedemption webPurchaseRedemption);
+
+    private ParseAsWebPurchaseRedemptionFunc ParseAsWebPurchaseRedemptionCallback { get; set; }
+
+    public void ParseAsWebPurchaseRedemption(string urlString, ParseAsWebPurchaseRedemptionFunc callback)
+    {
+        ParseAsWebPurchaseRedemptionCallback = callback;
+        _wrapper.ParseAsWebPurchaseRedemption(urlString);
+    }
+
+    public delegate void RedeemWebPurchaseFunc(WebPurchaseRedemptionResult result);
+
+    private RedeemWebPurchaseFunc RedeemWebPurchaseCallback { get; set; }
+
+    public void RedeemWebPurchase(WebPurchaseRedemption webPurchaseRedemption, RedeemWebPurchaseFunc callback)
+    {
+        RedeemWebPurchaseCallback = callback;
+        _wrapper.RedeemWebPurchase(webPurchaseRedemption);
+    }
+
     // ReSharper disable once UnusedMember.Local
     private void _receiveProducts(string productsJson)
     {
@@ -1528,6 +1548,48 @@ public partial class Purchases : MonoBehaviour
         }
 
         GetPromotionalOfferCallback = null;
+    }
+
+    private void _parseAsWebPurchaseRedemption(string parseAsWebPurchaseRedemptionJSON)
+    {
+        Debug.Log("_parseAsWebPurchaseRedemption " + parseAsWebPurchaseRedemptionJSON);
+
+        if (ParseAsWebPurchaseRedemptionCallback == null) return;
+
+        var response = JSON.Parse(parseAsWebPurchaseRedemptionJSON);
+
+        if (ResponseHasError(response))
+        {
+            ParseAsWebPurchaseRedemptionCallback(null);
+        }
+        else
+        {
+            var webPurchaseRedemption = new WebPurchaseRedemption(response["redemptionLink"]);
+            ParseAsWebPurchaseRedemptionCallback(webPurchaseRedemption);
+        }
+
+        ParseAsWebPurchaseRedemptionCallback = null;
+    }
+
+    private void _redeemWebPurchase(string redeemWebPurchaseJSON)
+    {
+        Debug.Log("_redeemWebPurchase " + redeemWebPurchaseJSON);
+
+        if (RedeemWebPurchaseCallback == null) return;
+
+        var response = JSON.Parse(redeemWebPurchaseJSON);
+
+        if (ResponseHasError(response))
+        {
+            RedeemWebPurchaseCallback(null);
+        }
+        else
+        {
+            var result = WebPurchaseRedemptionResult.FromJson(response);
+            RedeemWebPurchaseCallback(result);
+        }
+
+        RedeemWebPurchaseCallback = null;
     }
 
     private static void ReceiveCustomerInfoMethod(string arguments, CustomerInfoFunc callback)

RevenueCat/Scripts/PurchasesWrapper.cs
@@ -77,4 +77,6 @@ public interface IPurchasesWrapper
     void CanMakePayments(Purchases.BillingFeature[] features);
     void GetPromotionalOffer(string productIdentifier, string discountIdentifier);
     void ShowInAppMessages(Purchases.InAppMessageType[] messageTypes);
+    void ParseAsWebPurchaseRedemption(string urlString);
+    void RedeemWebPurchase(Purchases.WebPurchaseRedemption webPurchaseRedemption);
 }

RevenueCat/Scripts/PurchasesWrapperAndroid.cs
@@ -375,6 +375,16 @@ public class PurchasesWrapperAndroid : IPurchasesWrapper
         CallPurchases("showInAppMessages", JsonUtility.ToJson(request));
     }
 
+    public void ParseAsWebPurchaseRedemption(string urlString)
+    {
+        CallPurchases("parseAsWebPurchaseRedemption", urlString);
+    }
+
+    public void RedeemWebPurchase(Purchases.WebPurchaseRedemption webPurchaseRedemption)
+    {
+        CallPurchases("redeemWebPurchase", webPurchaseRedemption.RedemptionLink);
+    }
+
     private const string PurchasesWrapper = "com.revenuecat.purchasesunity.PurchasesWrapper";
 
     private static void CallPurchases(string methodName, params object[] args)

RevenueCat/Scripts/PurchasesWrapperNoop.cs
@@ -238,5 +238,13 @@ public partial class Purchases
         public void ShowInAppMessages(Purchases.InAppMessageType[] messageTypes)
         {
         }
+
+        public void ParseAsWebPurchaseRedemption(string urlString)
+        {
+        }
+
+        public void RedeemWebPurchase(Purchases.WebPurchaseRedemption webPurchaseRedemption)
+        {
+        }
     }
 }

RevenueCat/Scripts/PurchasesWrapperiOS.cs
@@ -454,5 +454,19 @@ public class PurchasesWrapperiOS : IPurchasesWrapper
 
         _RCShowInAppMessages(JsonUtility.ToJson(request));
     }
+
+    [DllImport("__Internal")]
+    private static extern void _RCParseAsWebPurchaseRedemption(string urlString);
+    public void ParseAsWebPurchaseRedemption(string urlString)
+    {
+        _RCParseAsWebPurchaseRedemption(urlString);
+    }
+
+    [DllImport("__Internal")]
+    private static extern void _RCRedeemWebPurchase(string resultJson);
+    public void RedeemWebPurchase(Purchases.WebPurchaseRedemption webPurchaseRedemption)
+    {
+        _RCRedeemWebPurchase(webPurchaseRedemption.RedemptionLink);
+    }
 }
 #endif

commit 662698449e05315f7ac409bef636b49973907b88

Fix Android SDK build issue (#546)
There was an issue building the SDK in android introduced in #539 and
released in #545.

I'm not sure why I didn't catch it when testing myself and it wasn't
caught by the tests... I believe I might have messed up a rebase. In any
case, I've tested the full redemption flow again with this change and it
works as expected...

Thanks to @fire-at-will for noticing and suggesting the fix 🙏

RevenueCat/Plugins/Android/PurchasesWrapper.java
@@ -571,7 +571,11 @@ public class PurchasesWrapper {
         boolean isWebPurchaseRedemptionURL = CommonKt.isWebPurchaseRedemptionURL(urlString);
         if (isWebPurchaseRedemptionURL) {
             JSONObject object = new JSONObject();
-            object.put("redemptionLink", urlString);
+            try {
+                object.put("redemptionLink", urlString);
+            } catch (JSONException e) {
+                logJSONException(e);
+            }
             sendJSONObject(object, PARSE_AS_WEB_PURCHASE_REDEMPTION);
         } else {
             sendJSONObject(null, PARSE_AS_WEB_PURCHASE_REDEMPTION);
@@ -582,11 +586,7 @@ public class PurchasesWrapper {
         CommonKt.redeemWebPurchase(redemptionLink, new OnResult() {
             @Override
             public void onReceived(Map<String, ?> map) {
-                try {
-                    sendJSONObject(MappersHelpersKt.convertToJson(map), REDEEM_WEB_PURCHASE);
-                } catch (JSONException e) {
-                    logJSONException(e);
-                }
+                sendJSONObject(MappersHelpersKt.convertToJson(map), REDEEM_WEB_PURCHASE);
             }
 
             @Override