953ae30002e58a17c4ef68a9391fed7cd72b4680
Will Taylor <wtaylor151@gmail.com>
2025-07-28 14:19:32 -0500

    add models3

---
+++ RevenueCat/Scripts/VirtualCurrencies.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using RevenueCat.SimpleJSON;
+
+public partial class Purchases
+{
+    /// <summary>
+    /// The VirtualCurrencies object contains all the virtual currencies associated to the user.
+    /// </summary>
+    public class VirtualCurrencies
+    {
+        /// <summary>
+        /// Map of all VirtualCurrency objects keyed by virtual currency code.
+        /// </summary>
+        public readonly Dictionary<string, VirtualCurrency> All;
+
+        public VirtualCurrencies(JSONNode response)
+        {
+            All = new Dictionary<string, VirtualCurrency>();
+            foreach (var keyValuePair in response["all"])
+            {
+                All.Add(keyValuePair.Key, new VirtualCurrency(keyValuePair.Value));
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(All)}:\n{DictToString(All)}";
+        }
+    }
+}
