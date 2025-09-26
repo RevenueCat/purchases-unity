82643ac26dc6368250bc702ee6ac78367c5047cf
Facundo Menzella <facumenzella@users.noreply.github.com>
2025-09-26 02:57:01 -0700

    Paywalls iOS (#675)
    This is a new implementation for
    https://github.com/RevenueCat/purchases-unity/pull/671
    
    This PR implements showing Paywalls for iOS, based on
    https://github.com/RevenueCat/purchases-unity/pull/649

--- RevenueCatUI/Plugins/iOS/RevenueCatUI.m
+++ RevenueCatUI/Plugins/iOS/RevenueCatUI.m
@@ -1,31 +1,162 @@
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+@import RevenueCat;
+@import PurchasesHybridCommonUI;
+#import <PurchasesHybridCommonUI/PurchasesHybridCommonUI-Swift.h>
 
-// Minimal native stubs for iOS bridging
+typedef void (*RCUIPaywallResultCallback)(const char *result);
 
-typedef void (*RCUIPaywallResultCallback)(const char* result);
+static NSString *const kRCUIOptionRequiredEntitlementIdentifier = @"requiredEntitlementIdentifier";
+static NSString *const kRCUIOptionOfferingIdentifier = @"offeringIdentifier";
+static NSString *const kRCUIOptionDisplayCloseButton = @"displayCloseButton";
 
-void rcui_presentPaywall(const char* offeringIdentifier, bool displayCloseButton, RCUIPaywallResultCallback callback) {
-    NSLog(@"[RevenueCatUI][iOS] presentPaywall(offering=%@, closeButton=%@)",
-          offeringIdentifier ? [NSString stringWithUTF8String:offeringIdentifier] : @"<null>",
-          displayCloseButton ? @"true" : @"false");
-    if (callback) {
-        callback("CANCELLED|Stub: no native UI");
+static NSString *RCUIStringFromCString(const char *string) {
+    if (string == NULL) {
+        return nil;
     }
+    return [NSString stringWithUTF8String:string];
 }
 
-void rcui_presentPaywallIfNeeded(const char* requiredEntitlementIdentifier, const char* offeringIdentifier, bool displayCloseButton, RCUIPaywallResultCallback callback) {
-    NSLog(@"[RevenueCatUI][iOS] presentPaywallIfNeeded(entitlement=%@, offering=%@, closeButton=%@)",
-          requiredEntitlementIdentifier ? [NSString stringWithUTF8String:requiredEntitlementIdentifier] : @"<null>",
-          offeringIdentifier ? [NSString stringWithUTF8String:offeringIdentifier] : @"<null>",
-          displayCloseButton ? @"true" : @"false");
-    if (callback) {
-        callback("NOT_PRESENTED|Stub: no native UI");
+static NSString *RCUINormalizedResultToken(NSString *resultName) {
+    if (resultName.length == 0) {
+        return @"ERROR";
     }
+
+    NSString *token = [resultName uppercaseString];
+
+    if ([token isEqualToString:@"PURCHASED"]) {
+        return @"PURCHASED";
+    }
+    if ([token isEqualToString:@"RESTORED"]) {
+        return @"RESTORED";
+    }
+    if ([token isEqualToString:@"CANCELLED"] || [token isEqualToString:@"USER_CANCELLED"]) {
+        return @"CANCELLED";
+    }
+    if ([token isEqualToString:@"NOT_PRESENTED"]) {
+        return @"NOT_PRESENTED";
+    }
+    if ([token isEqualToString:@"ERROR"] || [token isEqualToString:@"FAILED"]) {
+        return @"ERROR";
+    }
+
+    return token;
 }
 
-// No Customer Center in this stub
+static BOOL RCUIRuntimeSupportsPaywalls(void) {
+    if (@available(iOS 15.0, *)) {
+        return YES;
+    }
+    return NO;
+}
 
-bool rcui_isSupported() {
-    NSLog(@"[RevenueCatUI][iOS] isSupported() -> true (stub)");
-    return true;
+static void RCUIInvokeCallback(RCUIPaywallResultCallback callback, NSString *token, NSString *message) {
+    if (callback == NULL) {
+        return;
+    }
+
+    const char *payload = (token ?: @"ERROR").UTF8String;
+
+    dispatch_async(dispatch_get_main_queue(), ^{
+        callback(payload);
+    });
+}
+
+static BOOL RCUIEnsureReady(RCUIPaywallResultCallback callback) {
+    if (!RCUIRuntimeSupportsPaywalls()) {
+        RCUIInvokeCallback(callback, @"NOT_PRESENTED", @"Requires iOS 15.0+");
+        return NO;
+    }
+
+    if (!RCPurchases.isConfigured) {
+        RCUIInvokeCallback(callback, @"ERROR", @"PurchasesNotConfigured");
+        return NO;
+    }
+
+    return YES;
+}
+
+static NSMutableDictionary *RCUICreateOptionsDictionary(NSString *offeringIdentifier, BOOL displayCloseButton) {
+    NSMutableDictionary *options = [NSMutableDictionary new];
+    options[kRCUIOptionDisplayCloseButton] = @(displayCloseButton);
+
+    if (offeringIdentifier.length > 0) {
+        options[kRCUIOptionOfferingIdentifier] = offeringIdentifier;
+    }
+
+    return options;
+}
+
+static void RCUIPresentPaywallInternal(NSString *offeringIdentifier, BOOL displayCloseButton, RCUIPaywallResultCallback callback) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (@available(iOS 15.0, *)) {
+            __block PaywallProxy *proxy = [[PaywallProxy alloc] init];
+
+            NSMutableDictionary *options = RCUICreateOptionsDictionary(offeringIdentifier, displayCloseButton);
+
+            [proxy presentPaywallWithOptions:options
+                         paywallResultHandler:^(NSString * _Nonnull resultName) {
+                NSString *token = RCUINormalizedResultToken(resultName);
+                RCUIInvokeCallback(callback, token, nil);
+                proxy = nil;
+            }];
+        } else {
+            RCUIInvokeCallback(callback, @"NOT_PRESENTED", @"Requires iOS 15.0+");
+        }
+    });
+}
+
+static void RCUIPresentPaywallIfNeededInternal(NSString *requiredEntitlementIdentifier,
+                                               NSString *offeringIdentifier,
+                                               BOOL displayCloseButton,
+                                               RCUIPaywallResultCallback callback) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        if (@available(iOS 15.0, *)) {
+            __block PaywallProxy *proxy = [[PaywallProxy alloc] init];
+
+            NSMutableDictionary *options = RCUICreateOptionsDictionary(offeringIdentifier, displayCloseButton);
+            options[kRCUIOptionRequiredEntitlementIdentifier] = requiredEntitlementIdentifier;
+
+            [proxy presentPaywallIfNeededWithOptions:options
+                                  paywallResultHandler:^(NSString * _Nonnull resultName) {
+                NSString *token = RCUINormalizedResultToken(resultName);
+                RCUIInvokeCallback(callback, token, nil);
+                proxy = nil;
+            }];
+        } else {
+            RCUIInvokeCallback(callback, @"NOT_PRESENTED", @"Requires iOS 15.0+");
+        }
+    });
+}
+
+void rcui_presentPaywall(const char *offeringIdentifier, bool displayCloseButton, RCUIPaywallResultCallback callback) {
+    if (!RCUIEnsureReady(callback)) {
+        return;
+    }
+
+    NSString *offering = RCUIStringFromCString(offeringIdentifier);
+    RCUIPresentPaywallInternal(offering, displayCloseButton ? YES : NO, callback);
+}
+
+void rcui_presentPaywallIfNeeded(const char *requiredEntitlementIdentifier,
+                                 const char *offeringIdentifier,
+                                 bool displayCloseButton,
+                                 RCUIPaywallResultCallback callback) {
+    if (!RCUIEnsureReady(callback)) {
+        return;
+    }
+
+    NSString *entitlement = RCUIStringFromCString(requiredEntitlementIdentifier);
+    NSString *offering = RCUIStringFromCString(offeringIdentifier);
+
+    if (entitlement.length == 0) {
+        RCUIPresentPaywallInternal(offering, displayCloseButton ? YES : NO, callback);
+        return;
+    }
+
+    RCUIPresentPaywallIfNeededInternal(entitlement, offering, displayCloseButton ? YES : NO, callback);
+}
+
+bool rcui_isSupported(void) {
+    return RCUIRuntimeSupportsPaywalls();
 }
