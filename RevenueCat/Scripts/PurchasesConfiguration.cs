RevenueCat/purchases-unity

commit 07f6f251e1a005b9072c3635313ff16cfe92684f

Add support new native majors (#485)
This bumps PHC to 13.0.0 which includes breaking changes in the native
SDKs:
- iOS v5: https://github.com/RevenueCat/purchases-ios/releases/tag/5.0.0
- Android v8:
https://github.com/RevenueCat/purchases-android/releases/tag/8.0.0

RevenueCat/Scripts/PurchasesConfiguration.cs
@@ -25,26 +25,29 @@ public partial class Purchases
         public readonly string ApiKey;
         public readonly string AppUserId;
         public readonly bool ObserverMode;
+        public readonly PurchasesAreCompletedBy PurchasesAreCompletedBy;
         public readonly string UserDefaultsSuiteName;
         public readonly bool UseAmazon;
         public readonly DangerousSettings DangerousSettings;
-        public readonly bool UsesStoreKit2IfAvailable;
+        public readonly StoreKitVersion StoreKitVersion;
         public readonly bool ShouldShowInAppMessagesAutomatically;
         public readonly EntitlementVerificationMode EntitlementVerificationMode;
+        public readonly bool PendingTransactionsForPrepaidPlansEnabled;
 
-        private PurchasesConfiguration(string apiKey, string appUserId, bool observerMode, string userDefaultsSuiteName,
-            bool useAmazon, DangerousSettings dangerousSettings, bool usesStoreKit2IfAvailable, bool shouldShowInAppMessagesAutomatically, 
-            EntitlementVerificationMode entitlementVerificationMode)
+        private PurchasesConfiguration(string apiKey, string appUserId, PurchasesAreCompletedBy purchasesAreCompletedBy, string userDefaultsSuiteName,
+            bool useAmazon, DangerousSettings dangerousSettings, StoreKitVersion storeKitVersion, bool shouldShowInAppMessagesAutomatically, 
+            EntitlementVerificationMode entitlementVerificationMode, bool pendingTransactionsForPrepaidPlansEnabled)
         {
             ApiKey = apiKey;
             AppUserId = appUserId;
-            ObserverMode = observerMode;
+            PurchasesAreCompletedBy = purchasesAreCompletedBy;
             UserDefaultsSuiteName = userDefaultsSuiteName;
             UseAmazon = useAmazon;
             DangerousSettings = dangerousSettings;
-            UsesStoreKit2IfAvailable = usesStoreKit2IfAvailable;
+            StoreKitVersion = storeKitVersion;
             ShouldShowInAppMessagesAutomatically = shouldShowInAppMessagesAutomatically;
             EntitlementVerificationMode = entitlementVerificationMode;
+            PendingTransactionsForPrepaidPlansEnabled = pendingTransactionsForPrepaidPlansEnabled;
         }
 
         /// <summary>
@@ -70,13 +73,14 @@ public partial class Purchases
         {
             private readonly string _apiKey;
             private string _appUserId;
-            private bool _observerMode;
+            private PurchasesAreCompletedBy _purchasesAreCompletedBy;
             private string _userDefaultsSuiteName;
             private bool _useAmazon;
             private DangerousSettings _dangerousSettings;
-            private bool _usesStoreKit2IfAvailable;
+            private StoreKitVersion _storeKitVersion;
             private bool _shouldShowInAppMessagesAutomatically;
             private EntitlementVerificationMode _entitlementVerificationMode;
+            private bool _pendingTransactionsForPrepaidPlansEnabled;
 
             private Builder(string apiKey)
             {
@@ -91,9 +95,9 @@ public partial class Purchases
             public PurchasesConfiguration Build()
             {
                 _dangerousSettings = _dangerousSettings ?? new DangerousSettings(false);
-                return new PurchasesConfiguration(_apiKey, _appUserId, _observerMode, _userDefaultsSuiteName,
-                    _useAmazon, _dangerousSettings, _usesStoreKit2IfAvailable, _shouldShowInAppMessagesAutomatically, 
-                    _entitlementVerificationMode);
+                return new PurchasesConfiguration(_apiKey, _appUserId, _purchasesAreCompletedBy, _userDefaultsSuiteName,
+                    _useAmazon, _dangerousSettings, _storeKitVersion, _shouldShowInAppMessagesAutomatically, 
+                    _entitlementVerificationMode, _pendingTransactionsForPrepaidPlansEnabled);
             }
 
             public Builder SetAppUserId(string appUserId)
@@ -102,9 +106,10 @@ public partial class Purchases
                 return this;
             }
 
-            public Builder SetObserverMode(bool observerMode)
+            public Builder SetPurchasesAreCompletedBy(PurchasesAreCompletedBy purchasesAreCompletedBy, StoreKitVersion storeKitVersion)
             {
-                _observerMode = observerMode;
+                _purchasesAreCompletedBy = purchasesAreCompletedBy;
+                _storeKitVersion = storeKitVersion;
                 return this;
             }
 
@@ -126,15 +131,9 @@ public partial class Purchases
                 return this;
             }
 
-            [Obsolete("RevenueCat currently uses StoreKit 1 for purchases, as its stability in production " +
-                      "scenarios has proven to be more performant than StoreKit 2.\n" +
-                      "We're collecting more data on the best approach, but StoreKit 1 vs StoreKit 2 is \n" +
-                      "an implementation detail that you shouldn't need to care about.\n" +
-                      "We recommend not using this parameter, letting RevenueCat decide for " +
-                      "you which StoreKit implementation to use.", false)]
-            public Builder SetUsesStoreKit2IfAvailable(bool usesStoreKit2IfAvailable)
+            public Builder SetStoreKitVersion(StoreKitVersion storeKitVersion)
             {
-                _usesStoreKit2IfAvailable = usesStoreKit2IfAvailable;
+                _storeKitVersion = storeKitVersion;
                 return this;
             }
 
@@ -150,6 +149,12 @@ public partial class Purchases
                 return this;
             }
 
+            public Builder SetPendingTransactionsForPrepaidPlansEnabled(bool pendingTransactionsForPrepaidPlansEnabled)
+            {
+                _pendingTransactionsForPrepaidPlansEnabled = pendingTransactionsForPrepaidPlansEnabled;
+                return this;
+            }
+
         }
 
         public override string ToString()
@@ -157,13 +162,14 @@ public partial class Purchases
             return
                 $"{nameof(ApiKey)}: {ApiKey}\n" +
                 $"{nameof(AppUserId)}: {AppUserId}\n" +
-                $"{nameof(ObserverMode)}: {ObserverMode}\n" +
+                $"{nameof(PurchasesAreCompletedBy)}: {PurchasesAreCompletedBy}\n" +
                 $"{nameof(UserDefaultsSuiteName)}: {UserDefaultsSuiteName}\n" +
                 $"{nameof(UseAmazon)}: {UseAmazon}\n" +
                 $"{nameof(DangerousSettings)}: {DangerousSettings}\n" +
-                $"{nameof(UsesStoreKit2IfAvailable)}: {UsesStoreKit2IfAvailable}\n" +
+                $"{nameof(StoreKitVersion)}: {StoreKitVersion}\n" +
                 $"{nameof(ShouldShowInAppMessagesAutomatically)}: {ShouldShowInAppMessagesAutomatically}\n" +
-                $"{nameof(EntitlementVerificationMode)}: {EntitlementVerificationMode}";
+                $"{nameof(EntitlementVerificationMode)}: {EntitlementVerificationMode}\n" +
+                $"{nameof(PendingTransactionsForPrepaidPlansEnabled)}: {PendingTransactionsForPrepaidPlansEnabled}\n";
         }
     }
 }

.circleci/config.yml
@@ -303,7 +303,7 @@ jobs:
       - run:
           name: Install Pods
           working_directory: IntegrationTests/buildiOS
-          command: pod install --verbose
+          command: pod install --repo-update --verbose
 
       - save_cache:
           key: 1-pods-{{ checksum "RevenueCat/Plugins/Editor/RevenueCatDependencies.xml" }}

IntegrationTests/Assets/APITests/PurchasesAPITests.cs
@@ -27,12 +27,12 @@ public class PurchasesAPITests : MonoBehaviour
         };
 
         purchases.listener = new CustomListener();
-        purchases.observerMode = true;
         purchases.userDefaultsSuiteName = "suitename";
         purchases.proxyURL = "https://proxy-url.revenuecat.com";
 
         Purchases.CustomerInfo receivedCustomerInfo;
         Purchases.Error receivedError;
+        Purchases.StoreTransaction receivedTransaction;
 
         Purchases.Package receivedPackage;
         Purchases.Offerings receivedOfferings;
@@ -147,7 +147,6 @@ public class PurchasesAPITests : MonoBehaviour
             receivedError = error;
         });
 
-        purchases.SetFinishTransactions(true);
         #pragma warning disable CS0618 // Type or member is obsolete
         purchases.SetAllowSharingStoreAccount(false);
         #pragma warning restore CS0618 // Type or member is obsolete
@@ -170,7 +169,6 @@ public class PurchasesAPITests : MonoBehaviour
         });
 
         purchases.SyncPurchases();
-        purchases.SetAutomaticAppleSearchAdsAttributionCollection(true);
         purchases.EnableAdServicesAttributionTokenCollection();
         Dictionary<string, Purchases.IntroEligibility> receivedEligibilities;
         purchases.CheckTrialOrIntroductoryPriceEligibility(new string[] { "a", "b" },
@@ -220,16 +218,23 @@ public class PurchasesAPITests : MonoBehaviour
         Purchases.PurchasesConfiguration purchasesConfiguration =
             builder.SetUserDefaultsSuiteName("user_default")
             .SetDangerousSettings(new Purchases.DangerousSettings(false))
-            .SetObserverMode(true)
+            .SetPurchasesAreCompletedBy(Purchases.PurchasesAreCompletedBy.MyApp, Purchases.StoreKitVersion.StoreKit2)
             .SetUseAmazon(false)
             .SetAppUserId(appUserId)
-            .SetUsesStoreKit2IfAvailable(false)
+            .SetStoreKitVersion(Purchases.StoreKitVersion.StoreKit2)
             .SetShouldShowInAppMessagesAutomatically(false)
             .SetEntitlementVerificationMode(Purchases.EntitlementVerificationMode.Informational)
+            .SetPendingTransactionsForPrepaidPlansEnabled(true)
             .Build();
         purchases.Configure(purchasesConfiguration);
+        purchases.RecordPurchase("product_id", (transaction, error) => 
+        {
+            receivedTransaction = transaction;
+            receivedError = error;
+        });
 
         purchases.SyncObserverModeAmazonPurchase("product_id", "receipt_id", "amazon_user_id", "iso_currency_code", 1.99);
+        purchases.SyncAmazonPurchase("product_id", "receipt_id", "amazon_user_id", "iso_currency_code", 1.99);
 
         purchases.ShowInAppMessages(new Purchases.InAppMessageType[] { Purchases.InAppMessageType.BillingIssue,
             Purchases.InAppMessageType.PriceIncreaseConsent, Purchases.InAppMessageType.Generic });

IntegrationTests/Assets/APITests/SubscriptionOptionAPITests.cs
@@ -29,6 +29,9 @@ namespace DefaultNamespace
             bool IsBasePlan = subscriptionOption.IsBasePlan;
             bool IsPrepaid =  subscriptionOption.IsPrepaid;
             string PresentedOfferingIdentifier = subscriptionOption.PresentedOfferingIdentifier;
+            Purchases.SubscriptionOption.InstallmentsInfo installmentsInfo = subscriptionOption.OptionInstallmentsInfo;
+            int commitmentPaymentsCount = installmentsInfo.CommitmentPaymentsCount;
+            int renewalCommitmentPaymentsCount = installmentsInfo.RenewalCommitmentPaymentsCount;
         }
     }
 }

IntegrationTests/ProjectSettings/ProjectSettings.asset
@@ -178,10 +178,10 @@ PlayerSettings:
   StripUnusedMeshComponents: 1
   VertexChannelCompressionMask: 4054
   iPhoneSdkVersion: 988
-  iOSTargetOSVersionString: 11.0
+  iOSTargetOSVersionString: 13.0
   tvOSSdkVersion: 0
   tvOSRequireExtendedGameController: 0
-  tvOSTargetOSVersionString: 11.0
+  tvOSTargetOSVersionString: 13.0
   uIPrerenderedIcon: 0
   uIRequiresPersistentWiFi: 0
   uIRequiresFullScreen: 1

RevenueCat/Plugins/Android/PurchasesWrapper.java
@@ -8,7 +8,6 @@ import androidx.annotation.Nullable;
 import com.revenuecat.purchases.CustomerInfo;
 import com.revenuecat.purchases.DangerousSettings;
 import com.revenuecat.purchases.Purchases;
-import com.revenuecat.purchases.PurchasesAreCompletedBy;
 import com.revenuecat.purchases.Store;
 import com.revenuecat.purchases.common.PlatformInfo;
 import com.revenuecat.purchases.hybridcommon.CommonKt;
@@ -65,20 +64,20 @@ public class PurchasesWrapper {
     public static void setup(String apiKey,
                              String appUserId,
                              String gameObject,
-                             boolean observerMode,
+                             String purchasesAreCompletedBy,
                              String userDefaultsSuiteName,
                              boolean useAmazon,
                              boolean shouldShowInAppMessagesAutomatically,
                              String dangerousSettingsJSON,
-                             String entitlementVerificationMode) {
+                             String entitlementVerificationMode,
+                             boolean pendingTransactionsForPrepaidPlansEnabled) {
         PurchasesWrapper.gameObject = gameObject;
         PlatformInfo platformInfo = new PlatformInfo(PLATFORM_NAME, PLUGIN_VERSION);
         Store store = useAmazon ? Store.AMAZON : Store.PLAY_STORE;
         DangerousSettings dangerousSettings = getDangerousSettingsFromJSON(dangerousSettingsJSON);
-        PurchasesAreCompletedBy completedBy = observerMode ?
-                PurchasesAreCompletedBy.MY_APP : PurchasesAreCompletedBy.REVENUECAT;
-        CommonKt.configure(UnityPlayer.currentActivity, apiKey, appUserId, completedBy, platformInfo, store,
-                dangerousSettings, shouldShowInAppMessagesAutomatically, entitlementVerificationMode);
+        CommonKt.configure(UnityPlayer.currentActivity, apiKey, appUserId, purchasesAreCompletedBy, platformInfo, store,
+                dangerousSettings, shouldShowInAppMessagesAutomatically, entitlementVerificationMode,
+                pendingTransactionsForPrepaidPlansEnabled);
         Purchases.getSharedInstance().setUpdatedCustomerInfoListener(listener);
     }
 
@@ -312,14 +311,14 @@ public class PurchasesWrapper {
         });
     }
 
-    public static void syncObserverModeAmazonPurchase(
+    public static void syncAmazonPurchase(
             String productID,
             String receiptID,
             String amazonUserID,
             String isoCurrencyCode,
             double price
     ) {
-        Purchases.getSharedInstance().syncObserverModeAmazonPurchase(productID, receiptID,
+        Purchases.getSharedInstance().syncAmazonPurchase(productID, receiptID,
                 amazonUserID, isoCurrencyCode, price);
     }
 
@@ -355,11 +354,6 @@ public class PurchasesWrapper {
 
     public static void getCustomerInfo() {
         CommonKt.getCustomerInfo(getCustomerInfoListener(GET_CUSTOMER_INFO));
-    }
-
-    public static void setFinishTransactions(boolean enabled) {
-        CommonKt.setPurchasesAreCompletedBy(enabled ?
-                PurchasesAreCompletedBy.REVENUECAT : PurchasesAreCompletedBy.MY_APP);
     }
 
     public static void syncPurchases() {

RevenueCat/Plugins/Editor/RevenueCatDependencies.xml
@@ -1,10 +1,10 @@
 <?xml version="1.0" encoding="utf-8"?>
 <dependencies>
     <androidPackages>
-        <androidPackage spec="com.revenuecat.purchases:purchases-hybrid-common:[11.1.0]" />
+        <androidPackage spec="com.revenuecat.purchases:purchases-hybrid-common:[13.0.0]" />
         <androidPackage spec="androidx.annotation:annotation:[1.2.0]" />
     </androidPackages>
     <iosPods>
-        <iosPod name="PurchasesHybridCommon" version="11.1.0" minTargetSdk="11.0"/>
+        <iosPod name="PurchasesHybridCommon" version="13.0.0" minTargetSdk="13.0"/>
     </iosPods>
 </dependencies>

RevenueCat/Plugins/iOS/PurchasesUnityHelper.m
@@ -24,6 +24,7 @@ static NSString *const CHECK_ELIGIBILITY = @"_checkTrialOrIntroductoryPriceEligi
 static NSString *const CAN_MAKE_PAYMENTS = @"_canMakePayments";
 static NSString *const GET_PROMOTIONAL_OFFER = @"_getPromotionalOffer";
 static NSString *const HANDLE_LOG = @"_handleLog";
+static NSString *const RECORD_PURCHASE = @"_recordPurchase";
 
 #pragma mark Utility Methods
 
@@ -61,8 +62,8 @@ char *makeStringCopy(NSString *nstring) {
 - (void)setupPurchases:(NSString *)apiKey
              appUserID:(nullable NSString *)appUserID
             gameObject:(NSString *)gameObject
-          observerMode:(BOOL)observerMode
-usesStoreKit2IfAvailable:(BOOL)usesStoreKit2IfAvailable
+purchasesAreCompletedBy:(NSString *)purchasesAreCompletedBy
+       storeKitVersion:(NSString *)storeKitVersion
  userDefaultsSuiteName:(nullable NSString *)userDefaultsSuiteName
  dangerousSettingsJson:(NSString *)dangerousSettingsJson
  shouldShowInAppMessagesAutomatically:(BOOL)shouldShowInAppMessagesAutomatically
@@ -86,11 +87,11 @@ usesStoreKit2IfAvailable:(BOOL)usesStoreKit2IfAvailable
 
     [RCPurchases configureWithAPIKey:apiKey
                            appUserID:appUserID
-             purchasesAreCompletedBy:(observerMode ? RCPurchasesAreCompletedByMyApp : RCPurchasesAreCompletedByRevenueCat)
+             purchasesAreCompletedBy:purchasesAreCompletedBy
                userDefaultsSuiteName:userDefaultsSuiteName
                       platformFlavor:self.platformFlavor
                platformFlavorVersion:self.platformFlavorVersion
-            usesStoreKit2IfAvailable:usesStoreKit2IfAvailable
+                     storeKitVersion:storeKitVersion
                    dangerousSettings:dangerousSettings
 shouldShowInAppMessagesAutomatically:shouldShowInAppMessagesAutomatically
                     verificationMode:entitlementVerificationMode];
@@ -236,18 +237,6 @@ signedDiscountTimestamp:(NSString *)signedDiscountTimestamp {
     [RCCommonFunctionality getCustomerInfoWithCompletionBlock:[self getCustomerInfoCompletionBlockFor:GET_CUSTOMER_INFO]];
 }
 
--  (void)setFinishTransactions:(BOOL)finishTransactions {
-    if (finishTransactions) {
-        [RCCommonFunctionality setPurchasesAreCompletedBy:RCPurchasesAreCompletedByRevenueCat];
-    } else {
-        [RCCommonFunctionality setPurchasesAreCompletedBy:RCPurchasesAreCompletedByMyApp];
-    }
-}
-
-- (void)setAutomaticAppleSearchAdsAttributionCollection:(BOOL)enabled {
-    [RCCommonFunctionality setAutomaticAppleSearchAdsAttributionCollection:enabled];
-}
-
 - (void)enableAdServicesAttributionTokenCollection {
     if (@available(iOS 14.3, macOS 11.1, macCatalyst 14.3, *)) {
         [RCCommonFunctionality enableAdServicesAttributionTokenCollection];
@@ -291,6 +280,19 @@ signedDiscountTimestamp:(NSString *)signedDiscountTimestamp {
      } else {
          NSLog(@"[Purchases] Warning: tried to present codeRedemptionSheet, but it's only available on iOS 14.0 or greater.");
      }
+}
+
+- (void)recordPurchase:(NSString *)productID {
+    [RCCommonFunctionality recordPurchaseForProductID:productID completion:^(NSDictionary *_Nullable responseDictionary, RCErrorContainer *_Nullable error) {
+        NSMutableDictionary *response = [NSMutableDictionary new];
+        if (error) {
+            response[@"error"] = error.info;
+        } else {
+            response[@"transaction"] = responseDictionary;
+        }
+
+        [self sendJSONObject:response toMethod:RECORD_PURCHASE];
+    }];
 }
 
 - (void)setSimulatesAskToBuyInSandbox:(BOOL)enabled {
@@ -507,8 +509,8 @@ static RCUnityHelperDelegate *_RCUnityHelperShared() {
 void _RCSetupPurchases(const char *gameObject,
                        const char *apiKey,
                        const char *appUserID,
-                       const BOOL observerMode,
-                       const BOOL usesStoreKit2IfAvailable,
+                       const char *purchasesAreCompletedBy,
+                       const char *storeKitVersion,
                        const char *userDefaultsSuiteName,
                        const char *dangerousSettingsJson,
                        const BOOL shouldShowInAppMessagesAutomatically,
@@ -516,8 +518,8 @@ void _RCSetupPurchases(const char *gameObject,
     [_RCUnityHelperShared() setupPurchases:convertCString(apiKey)
                                  appUserID:convertCString(appUserID)
                                 gameObject:convertCString(gameObject)
-                              observerMode:observerMode
-                  usesStoreKit2IfAvailable:usesStoreKit2IfAvailable
+                   purchasesAreCompletedBy:convertCString(purchasesAreCompletedBy)
+                           storeKitVersion:convertCString(storeKitVersion)
                      userDefaultsSuiteName:convertCString(userDefaultsSuiteName)
                      dangerousSettingsJson:convertCString(dangerousSettingsJson)
       shouldShowInAppMessagesAutomatically:shouldShowInAppMessagesAutomatically
@@ -571,9 +573,6 @@ void _RCLogOut() {
     [_RCUnityHelperShared() logOut];
 }
 
-void _RCSetFinishTransactions(const BOOL finishTransactions) {
-    [_RCUnityHelperShared() setFinishTransactions:finishTransactions];
-}
 void _RCSetAllowSharingStoreAccount(const BOOL allow) {
     [_RCUnityHelperShared() setAllowSharingStoreAccount:allow];
 }
@@ -606,6 +605,10 @@ void _RCSetProxyURLString(const char *proxyURLString) {
     [_RCUnityHelperShared() setProxyURLString:convertCString(proxyURLString)];
 }
 
+void _RCRecordPurchase(const char *productID) {
+    [_RCUnityHelperShared() recordPurchase:convertCString(productID)];
+}
+
 void _RCSetSimulatesAskToBuyInSandbox(const BOOL enabled) {
     [_RCUnityHelperShared() setSimulatesAskToBuyInSandbox:enabled];
 }
@@ -616,10 +619,6 @@ void _RCGetCustomerInfo() {
 
 char * _RCGetAppUserID() {
     return [_RCUnityHelperShared() getAppUserID];
-}
-
-void _RCSetAutomaticAppleSearchAdsAttributionCollection(const BOOL enabled) {
-    [_RCUnityHelperShared() setAutomaticAppleSearchAdsAttributionCollection:enabled];
 }
 
 void _RCEnableAdServicesAttributionTokenCollection() {

commit f7f53210bfd18259aa26ae79a2249975edc92cd0

fix: align PurchasesConfiguration.Builder defaults with the inspector
The builder left three fields at their C# zero values, so apps configuring
through runtime setup silently got StoreKit 1, store in-app messages
disabled, and entitlement verification disabled -- while the inspector
fields on Purchases default to StoreKitVersion.Default, in-app messages
enabled, and Informational. Build() passes these straight to the native
SDKs, so the divergence was user visible:

- StoreKit 1 is pinned rather than letting RevenueCat pick, which makes
  the win-back offer APIs fail on iOS 18+ (they require StoreKit 2).
  Broken since #485 replaced UsesStoreKit2IfAvailable with the enum.
- Store in-app messages are suppressed, so users with billing issues are
  never prompted to fix their payment method. The inspector tooltip has
  always documented the opposite. Broken since #338 added the setting.
- Trusted entitlements stayed Disabled, so #748's change of the default
  to Informational only ever reached inspector users.

Give the three fields explicit initializers matching the inspector, and
assert the documented values in BuildUsesDocumentedDefaults.

Co-Authored-By: Claude <noreply@anthropic.com>

RevenueCat/Scripts/PurchasesConfiguration.cs
@@ -84,9 +84,9 @@ public partial class Purchases
             private string _userDefaultsSuiteName;
             private bool _useAmazon;
             private DangerousSettings _dangerousSettings;
-            private StoreKitVersion _storeKitVersion;
-            private bool _shouldShowInAppMessagesAutomatically;
-            private EntitlementVerificationMode _entitlementVerificationMode;
+            private StoreKitVersion _storeKitVersion = StoreKitVersion.Default;
+            private bool _shouldShowInAppMessagesAutomatically = true;
+            private EntitlementVerificationMode _entitlementVerificationMode = EntitlementVerificationMode.Informational;
             private bool _pendingTransactionsForPrepaidPlansEnabled;
             private bool _diagnosticsEnabled;
             private bool _automaticDeviceIdentifierCollectionEnabled = true;

IntegrationTests/Assets/Tests/EditMode/PurchasesConfigurationTests.cs
@@ -5,16 +5,11 @@ namespace RevenueCat.Tests
     public class PurchasesConfigurationTests
     {
         /// <remarks>
-        /// This locks in what the builder does today, which is not what the SDK documents.
-        /// The builder leaves StoreKitVersion, ShouldShowInAppMessagesAutomatically and
-        /// EntitlementVerificationMode at their C# zero values, so the runtime-setup path defaults to
-        /// StoreKit 1, in-app messages disabled and verification disabled, while the inspector fields on
-        /// Purchases default to StoreKitVersion.Default, in-app messages enabled and Informational.
-        /// Those divergences look unintentional, but aligning the builder changes runtime behavior for
-        /// apps using runtime setup, so it is handled separately from these tests.
+        /// These must stay in sync with the inspector defaults on Purchases, so that configuring through
+        /// runtime setup and configuring through the inspector behave the same when a setter is not called.
         /// </remarks>
         [Test]
-        public void BuildUsesBuilderDefaults()
+        public void BuildUsesDocumentedDefaults()
         {
             var configuration = Purchases.PurchasesConfiguration.Builder
                 .Init("test_api_key")
@@ -26,12 +21,9 @@ namespace RevenueCat.Tests
             Assert.That(configuration.UserDefaultsSuiteName, Is.Null);
             Assert.That(configuration.UseAmazon, Is.False);
             Assert.That(configuration.DangerousSettings.AutoSyncPurchases, Is.True);
-            // Diverges from the Purchases inspector default (StoreKitVersion.Default).
-            Assert.That(configuration.StoreKitVersion, Is.EqualTo(Purchases.StoreKitVersion.StoreKit1));
-            // Diverges from the documented default (in-app messages are shown automatically).
-            Assert.That(configuration.ShouldShowInAppMessagesAutomatically, Is.False);
-            // Diverges from the Purchases inspector default (EntitlementVerificationMode.Informational).
-            Assert.That(configuration.EntitlementVerificationMode, Is.EqualTo(Purchases.EntitlementVerificationMode.Disabled));
+            Assert.That(configuration.StoreKitVersion, Is.EqualTo(Purchases.StoreKitVersion.Default));
+            Assert.That(configuration.ShouldShowInAppMessagesAutomatically, Is.True);
+            Assert.That(configuration.EntitlementVerificationMode, Is.EqualTo(Purchases.EntitlementVerificationMode.Informational));
             Assert.That(configuration.PendingTransactionsForPrepaidPlansEnabled, Is.False);
             Assert.That(configuration.DiagnosticsEnabled, Is.False);
             Assert.That(configuration.AutomaticDeviceIdentifierCollectionEnabled, Is.True);